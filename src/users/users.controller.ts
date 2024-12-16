ad3ff68f86fee237f55986f5b036658fdf9c588f
Malik786-git <malikmuhammadjahangir786@gmail.com>
2024-12-17 02:13:11 +0500

    add pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,38 +1,52 @@
-import { Controller, Get, Param, Post, Query, Body, Headers, Ip } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Param,
+	Post,
+	Query,
+	Body,
+	Headers,
+	Ip,
+	ParseIntPipe,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  public getUsers(): string {
-    return 'Get all users';
-  }
+	@Get()
+	public getUsers(): string {
+		return 'Get all users';
+	}
 
-  // ? mean id is optional api not show err
-  @Get('single/:id/:option?')
-  public getSingleUsers(@Param() params: any, @Query() query: any): string {
-    console.log(params);
-    console.log(query);
-    return 'Get single users';
-  }
-  // get each params n query separately
-  @Get('/more/single/:id/:option?')
-  public getMoreSpecificDataOfSingleUsers(
-    @Param('id') id: string,
-    @Param('option') option: string,
-    @Query('limit') limit: number,
-    @Query('offset') offset: number,
-  ): string {
-    console.log(id);
-    console.log(option);
-    console.log(limit);
-    console.log(offset);
-    return 'Get single users';
-  }
-  @Post()
-  public createPost(@Body() body: any, @Headers() headers:any, @Ip() ip:any): string {
-    console.log(body);
-    console.log(headers);
-    console.log(ip);
-    return 'user created successfully';
-  }
+	// ? mean id is optional api not show err
+	@Get('single/:id/:option?')
+	public getSingleUsers(@Param() params: any, @Query() query: any): string {
+		console.log(params);
+		console.log(query);
+		return 'Get single users';
+	}
+	// get each params n query separately
+	@Get('/more/single/:id/:option?')
+	public getMoreSpecificDataOfSingleUsers(
+		@Param('id') id: string,
+		@Param('option', ParseIntPipe) option: number | undefined,
+		@Query('limit') limit: string,
+		@Query('offset') offset: string,
+	): string {
+		console.log(typeof id);
+		console.log(typeof option); //Note: we get err if we get option param as a string, but if we not pass option key coz its optional param so it sill show validation err coz validation pipe only apply on req params
+		console.log(typeof limit);
+		console.log(typeof offset);
+		return 'Get single users';
+	}
+	@Post()
+	public createPost(
+		@Body() body: any,
+		@Headers() headers: any,
+		@Ip() ip: any,
+	): string {
+		console.log(body);
+		console.log(headers);
+		console.log(ip);
+		return 'user created successfully';
+	}
 }
